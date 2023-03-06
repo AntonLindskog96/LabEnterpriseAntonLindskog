b3ccc151800dd37d793af43f1680b9c8fcb532c9
Anton <lindskog96@live.se>
2023-03-06 09:44:48 +0100

    added mapper

--- src/main/java/com/example/labee/country/CountryMapper.java
+++ src/main/java/com/example/labee/country/CountryMapper.java
@@ -1,5 +1,20 @@
 package com.example.labee.country;
 
+import com.example.labee.country.dto.CountryDTO;
+import com.example.labee.country.entity.Country;
+
+import java.util.List;
+
 public class CountryMapper {
+
+    public List<CountryDTO> map(List<Country> countries) {
+
+        return countries.stream().map(country -> new CountryDTO(country.getId(), country.getName())).toList();
+    }
+
+    public CountryDTO map(Country country) {
+        return new CountryDTO(country.getId(), country.getName());
+    }
+
 
 }
