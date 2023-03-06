59ae99128484288be5b2723bf049fe6b2e55210c
Anton <lindskog96@live.se>
2023-03-06 11:32:46 +0100

    add Exception

---
+++ src/test/java/com/example/labee/country/Repository/CountryRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.labee.country.Repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+
+class CountryRepositoryTest {
+
+
+}
